9443435e21844090113606eadbd4e4f775554511
iweeell <mihailyukov06@gmail.com>
2024-09-09 17:19:08 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,72 @@
+const buttonSearch = document.querySelector('.info__btn');
+const inputSearch = document.querySelector('.info__input');
+const APIKey = 'a8a1a655f133583bc05c1575299a8ec8';
+
+function weather(e) {
+
+  fetch(e)
+    .then(response => response.json())
+    .then(data => {
+      const cityName = document.querySelector('.out__location-city');
+      const icon = document.querySelector('.out__icon')
+      const temp = document.querySelector('.out__temp');
+      const descr = document.querySelector('.info__descr');
+      const cloudy = document.querySelector('.cloudy');
+      const windSpeed = document.querySelector('.wind');
+      const maxTemp = document.querySelector('.max-temp');
+      const minTemp = document.querySelector('.min-temp');
+      const humadity = document.querySelector('.humadity');
+      const weatherBg = document.querySelector('.weather');
+  
+
+      cityName.textContent = data.name;
+      temp.innerHTML = `${Math.round(data.main.temp)}<span>°</span>`;
+      descr.textContent = data.weather[0].description;
+      cloudy.innerHTML = `${data.clouds.all}<span>%</span><img src="images/cloudy.svg" alt=" ">`;
+      windSpeed.innerHTML = `${Math.round((data.wind.speed / 1000) * 3600)}<span>km/h</span><img src="images/wind.svg" alt=" ">`;
+      icon.innerHTML = `<img src="images/icon/${data.weather[0].icon}.svg" alt=" ">`;
+      maxTemp.innerHTML = `${Math.round(data.main.temp_max)}<span>°</span><img src="images/temp-max.svg" alt=" ">`;
+      minTemp.innerHTML = `${Math.round(data.main.temp_min)}<span>°</span><img src="images/temp-min.svg" alt=" ">`;
+      humadity.innerHTML = `${data.main.humidity}<span>%</span><img src="images/humadity.svg" alt=" ">`;
+      weatherBg.setAttribute('style', `background-image: url('images/icon/${data.weather[0].icon}.jpg');`)
+      
+    })
+};
+
+
+function getLocatin() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    inputSearch.value = 'Kyiv';
+    buttonSearch.click();
+  }
+}
+
+function showPosition(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const city = `lat=${lat}&lon=${lon}`
+
+  weather(`https://api.openweathermap.org/data/2.5/weather?${city}&units=metric&appid=${APIKey}`)
+
+}
+
+buttonSearch.addEventListener('click', () => {
+  const city = inputSearch.value;
+  if (city === '') {
+    return
+  }
+  weather(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`);
+})
+
+inputSearch.addEventListener('keyup', (el) => {
+  if (el.keyCode === 13) {
+    buttonSearch.click();
+  }
+})
+
+window.addEventListener('load', () => {
+  getLocatin();
+})
+
